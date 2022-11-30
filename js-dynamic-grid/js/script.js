fa714cc79d6c3e283bee88bacb9de04d5ea403e5
RC2PC2 <petriccarcc@gmail.com>
2022-11-30 15:07:26 +0100

    dynamic grid update

--- js-dynamic-grid/js/script.js
+++ js-dynamic-grid/js/script.js
@@ -7,21 +7,19 @@ const gridContainer = document.querySelector('div.grid');
 // § 1.5 per ogni elemento dei 64 che voglio creare
 for (let  i = 0 ; i < 49 ; i++){
 
-   const newSquare = getMeANewSquare(parseInt(Math.random() * 100));
+   const newSquare = getMeANewSquare();
 
    // § 4 - aggiungo il div al parent
    gridContainer.appendChild(newSquare);
 }
 
 // funzione per creare un quadrato
-function getMeANewSquare(content){
+function getMeANewSquare(){
    // § 2 - creo il div
    const newSquare = document.createElement('div');
 
    // § 3 - gli attribuisco le proprietà che voglio
-   newSquare.classList.add('square', 'd-flex');
-
-   newSquare.innerHTML = `<span class="m-auto fs-2"> ${content} </span>`;
+   newSquare.classList.add('square');
 
    // ? 3.1 - tra le quali un comportamento sul click
    newSquare.addEventListener("click", function(){
